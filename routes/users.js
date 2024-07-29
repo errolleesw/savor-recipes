5fd3778ec246316e2cb015705a0ab2525b71daa1
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2024-07-29 22:56:19 +0400

    Add userController

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,23 @@
 import express from "express";
-// import passport from "passport";
-// import { registerUser, logoutUser } from "../controllers/userController.js";
+import passport from "passport";
+import { registerUser, logoutUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/sign-up", (req, res) =>
   res.render("sign-up-form", { title: "Sign up" })
 );
-// router.post("/sign-up", registerUser);
+router.post("/sign-up", registerUser);
 
-// router.get("/log-in", (req, res) => res.render("log-in", { title: "Log in" }));
-// router.post(
-//   "/log-in",
-//   passport.authenticate("local", {
-//     successRedirect: "/recipes",
-//     failureRedirect: "/log-in",
-//   })
-// );
+router.get("/log-in", (req, res) => res.render("log-in", { title: "Log in" }));
+router.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/recipes",
+    failureRedirect: "/log-in",
+  })
+);
 
-// router.get("/log-out", logoutUser);
+router.get("/log-out", logoutUser);
 
 export default router;
