249dbd9988554c5588b280f4d762717b4baaa960
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2024-07-29 22:44:27 +0400

    Added sign-up form

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(serveStatic(join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
